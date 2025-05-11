35a0d56c6e8435cf4278cdc211255d07d579676e
ryanawal12 <rawal@ucsd.edu>
2025-05-10 20:27:16 -0700

    style checker - done

--- test.js
+++ test.js
@@ -1,5 +1,27 @@
-// test.js - testing for syntax
-var badVariable = "double quotes";
-function testFunc(){
-console.log("this is a test function")
-testFunc();
+// Bad Example
+
+// Global variables using var (bad), mixed quotes, no semicolon, inconsistent indentation
+var userName = "Admin"
+function greetUser(username){
+  if(username == "Admin"){
+    console.log("Welcome, Admin")
+    console.log("You have full access to the system")
+  } else{
+      console.log("Hello, " + username + "!")
+  }
+}
+
+// Loose equality, no semicolon, mixed quotes, bad indentation
+function checkAccess(role){
+  if (role == "admin") 
+  {
+  console.log("Access granted")
+  } 
+  else 
+  {
+  console.log("Access denied")
+  }
+}
+
+greetUser(userName)
+checkAccess("admin")
