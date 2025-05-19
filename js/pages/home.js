5777189cfdecdb25687aa8986408ef5713c84982
Chanbin Na <chanbinna@gmail.com>
2025-05-18 18:25:28 -0700

    refactored

---
+++ js/pages/home.js
@@ -0,0 +1,50 @@
+export default function () {
+    return ` <section class="sub-nav">
+            <ul class="filters">
+                <li>
+                    <select class="btn-filter" name="filter1">
+                        <option disabled selected>Meal</option>
+                        <option>Breakfast</option>
+                        <option>Lunch</option>
+                        <option>Dinner</option>
+                        <option>Desserts</option>
+                        <option>Snacks</option>
+                        <option>Beverages</option>
+                    </select>
+                </li>
+                <li>
+                    <select class="btn-filter" name="filter2">
+                        <option disabled selected>Cuisine</option>
+                        <option>Asian</option>
+                        <option>European</option>
+                        <option>Latin American</option>
+                        <option>African</option>
+                        <option>Middle Eastern</option>
+                    </select>
+                </li>
+                <li>
+                    <select class="btn-filter" name="filter 3">
+                        <option disabled selected>Estimated Time</option>
+                        <option> Under 30 minutes </option>
+                        <option> Under 1 Hour</option>
+                        <option> Over 1 Hour</option>
+                    </select>
+                </li>
+                <li>
+                    <select class="btn-filter" name="filter4">
+                        <option disabled selected>Ingredients</option>
+                        <option>Meat</option>
+                        <option>Vegetables</option>
+                        <option>Dairy</option>
+                    </select>
+                </li>
+            </ul>
+
+            <!-- Action buttons (Shuffle/Add) -->
+            <div class="actions">
+                <button class="btn-shuffle" type="button">Shuffle</button>
+                <button class="btn-add" type="button">Add Recipe Card</button>
+            </div>
+
+        </section>`;
+}
