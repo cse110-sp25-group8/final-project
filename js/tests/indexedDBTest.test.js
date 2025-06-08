f231b5eaffe708bb931e9a801420e4904d260d8e
kiara-singh <singhkiara317@gmail.com>
2025-06-07 23:02:12 -0700

    Added getAllRecipes test

--- js/tests/indexedDBTest.test.js
+++ js/tests/indexedDBTest.test.js
@@ -13,8 +13,9 @@ let index = 0;
 
 // check if recipes are being added correctly
 test('addValidRecipe', async () => {
-    let data={Name: "Pie",
-              CookTime:  "30"};
+    let data={  id: 1,
+                Name: "Pie",
+                CookTime:  "30"};
     index++;
     let ans=await db.addRecipe(data);
     expect(ans).toBe(index);
@@ -22,11 +23,48 @@ test('addValidRecipe', async () => {
 
 // delete recipe
 test('DeleteRecipe', async () => {
-    console.log(typeof index);
-    let ans=await db.deleteRecipe(index);
+     let data={ id: 1,
+                Name: "Pie",
+                CookTime:  "30"};
+    await db.deleteRecipe(data);
+    let ans=await db.getRecipe(1);
     index--
-    expect(ans).toBe(index);
+    expect(ans).toBeUndefined();
 })
+
+//edit - doesn't effect count, before vs after of the field we edited, other fields are the same 
+
+
+//Valid getRecipe - does it return correct data, check the fields
+
+
+
+//get all recipes 
+test('Get all recipes in database', async()=>{
+    let data={  id: 1,
+                Name: "Pie",
+                CookTime:  "30"};
+    let data2={  id: 2,
+                    Name: "Soup",
+                    CookTime:  "40"};
+    await db.addRecipe(data);
+    await db.addRecipe(data2)
+    let res=[   {
+                    id: 1,
+                    Name: "Pie",
+                    CookTime:  "30",
+                },
+                {
+                    id: 2,
+                    Name: "Soup",
+                    CookTime:  "40",
+                }
+            ]
+    let ans=await db.getAllRecipes()
+    expect(ans).toEqual(res);
+
+})
+
 
 
 
