9a95ab0b4fdcf7a6548698c37b4f6f713e487ac2
asvo <asvo@ucsd.edu>
2025-06-08 09:51:20 -0700

    Organize test structure and add two tests (add and delete)

--- js/tests/indexedDBTest.test.js
+++ js/tests/indexedDBTest.test.js
@@ -1,69 +1,66 @@
+import { RecipeStore } from '../database/RecipeStore';
+import 'fake-indexeddb/auto';
+
 if (typeof global.structuredClone !== 'function') {
-  global.structuredClone = (val) => JSON.parse(JSON.stringify(val));
+    global.structuredClone = (val) => JSON.parse(JSON.stringify(val));
 }
 
-
-import { RecipeStore } from "../database/RecipeStore";
-import "fake-indexeddb/auto";
-// require("fake-indexeddb/auto");
-
-
 const db = new RecipeStore();
-let index = 0;
+const data = [
+    { id: 1, name: 'Pie', cookTime: '30' },
+    { id: 2, name: 'Soup', cookTime: '40' },
+    { id: 3, name: 'Noodles', cookTime: '60' },
+    { id: 4, name: 'Hamburger', cookTime: '50' },
+    { id: 5, name: 'Buns', cookTime: '25' }
+];
+let currentDBSize = 0;
 
-// check if recipes are being added correctly
-test('addValidRecipe', async () => {
-    let data={  id: 1,
-                Name: "Pie",
-                CookTime:  "30"};
-    index++;
-    let ans=await db.addRecipe(data);
-    expect(ans).toBe(index);
-})
+// Check if recipes are being added correctly
+describe('[C]RUD: Add to database tests', () => { 
+    test('Add a recipe to the database.', async () => {
+        let ans = await db.addRecipe(data[0]);
+        currentDBSize++;
+        expect(ans).toBe(currentDBSize);
+    });
 
-// delete recipe
-test('DeleteRecipe', async () => {
-     let data={ id: 1,
-                Name: "Pie",
-                CookTime:  "30"};
-    await db.deleteRecipe(data);
-    let ans=await db.getRecipe(1);
-    index--
-    expect(ans).toBeUndefined();
-})
+    test('Try to add no data to the database.', async () => {
+        await expect(db.addRecipe()).rejects.toBeInstanceOf(Error);
+    });
+});
 
-//edit - doesn't effect count, before vs after of the field we edited, other fields are the same 
+// Delete recipe
+describe('CRU[D]: Delete from database tests', () => {
+    test('Delete a recipe from the database.', async () => {
+        await db.deleteRecipe(data[0]);
+        currentDBSize--;
+
+        let ans = await db.getRecipe(1);
+        expect(ans).toBeUndefined();
+    });
+
+    test('Delete a non-existent recipe from the database.', async () => {
+        // Due to IndexedDB's behavior, no error is thrown if the target ID doesn't exist.
+        let badData = {id: 10, name: 'Potato Salad', cookTime: '15' };
+        await expect(db.deleteRecipe(badData)).resolves.toBeUndefined();
+    });
+});
+
+// Edit - doesn't effect count, before vs after of the field we edited, other fields are the same 
 
 
-//Valid getRecipe - does it return correct data, check the fields
+// Valid getRecipe - does it return correct data, check the fields
 
 
 
-//get all recipes 
-test('Get all recipes in database', async()=>{
-    let data={  id: 1,
-                Name: "Pie",
-                CookTime:  "30"};
-    let data2={  id: 2,
-                    Name: "Soup",
-                    CookTime:  "40"};
-    await db.addRecipe(data);
-    await db.addRecipe(data2)
-    let res=[   {
-                    id: 1,
-                    Name: "Pie",
-                    CookTime:  "30",
-                },
-                {
-                    id: 2,
-                    Name: "Soup",
-                    CookTime:  "40",
-                }
-            ]
-    let ans=await db.getAllRecipes()
+// Get all recipes 
+test('Get all recipes in database', async() => {
+    await db.addRecipe(data[0]);
+    await db.addRecipe(data[1]);
+
+    let res = [data[0], data[1]];
+    let ans = await db.getAllRecipes();
     expect(ans).toEqual(res);
-
-})
+});
 
 
 
